28abcb9919386e1d24ee78aa67557dab8deaa284
padd60 <padd60@naver.com>
2022-07-26 20:20:01 +0900

    feat : 스터디 상세 페이지 목데이터 추가 및 타입 정의

---
+++ src/interfaces/studyDetail.ts
@@ -0,0 +1,43 @@
+export interface detailMemberType {
+  profileImageUrl: string;
+  nickName: string;
+  field: string;
+  career: string;
+  mbti: string;
+}
+
+export interface detailStudyPeriodType {
+  startDate: string;
+  endDate: string;
+}
+
+export interface detailQuestionsChildrenType {
+  parentId: number;
+  questionId: number;
+  nickName: string;
+  content: string;
+  createdAt: string;
+}
+
+export interface detailQuestionsType {
+  questionId: number;
+  parentId: null;
+  nickName: string;
+  content: string;
+  createdAt: string;
+  children: detailQuestionsChildrenType[];
+}
+
+export interface studyDetailType {
+  topic: string;
+  title: string;
+  member: detailMemberType;
+  preferredMbtis: string[];
+  isOnline: boolean;
+  region: string;
+  studyPeriod: detailStudyPeriodType;
+  numberOfMembers: number;
+  numberOfRecruits: number;
+  description: string;
+  questions: detailQuestionsType[];
+}
