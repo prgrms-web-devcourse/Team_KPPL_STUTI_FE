13ac95abd16139b3d3134be50da48f3ca831c3fb
padd60 <padd60@naver.com>
2022-07-28 04:12:57 +0900

    feat : 스터디 제목 및 태그, 이미지 header 컴포넌트로 구현

---
+++ src/components/StudyDetail/DetailHeader/style.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const HeaderWrapper = styled.div`
+  width: 600px;
+  margin: 0 auto;
+`;
+
+export const ImageWrapper = styled.div`
+  width: 100%;
+  height: 400px;
+  margin-top: 20px;
+  background-color: #eee;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  object-position: center;
+`;
