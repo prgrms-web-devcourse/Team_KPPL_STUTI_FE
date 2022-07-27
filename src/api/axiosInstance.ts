5f11161abb4a32e14381b9d7b1dc600c278fe66d
padd60 <padd60@naver.com>
2022-07-28 04:11:14 +0900

    feat, refactor : api 주소 수정 및 스터디 상세페이지 api get 함수 추가

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance } from 'axios';
 const host = process.env.REACT_APP_API_HOST ?? 'localhost';
 const port = process.env.REACT_APP_API_PORT ?? 3000;
 
-const API_ENDPOINT = `${host}:${port}`;
+const API_ENDPOINT = `http://${host}:${port}`;
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: API_ENDPOINT, // baseURL 미리세팅
---
+++ src/api/studyDetail.ts
@@ -0,0 +1,13 @@
+import axiosInstance from '@src/api/axiosInstance';
+
+export const getStudyDetailInfomation = async () => {
+  try {
+    const { data } = await axiosInstance({
+      url: '/mock/studyDetailMockData.json',
+      method: 'GET',
+    });
+    return data;
+  } catch (error) {
+    new Error('스터디 상세 정보를 가져오는 도중에 오류가 발생했습니다!');
+  }
+};
