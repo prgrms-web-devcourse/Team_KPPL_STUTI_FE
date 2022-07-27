c0466010bf0ca377c9fb7fd7cbf801d579458db9
lexie-kaia <kaia.gayeon.kim@gmail.com>
2022-07-27 19:30:58 +0900

    refactor: 리뷰 반영

--- src/components/MbtiTag/MbtiTag.tsx
+++ src/components/MbtiTag/MbtiTag.tsx
@@ -3,10 +3,11 @@ import { CustomChip } from './MbtiTag.style';
 export interface Props {
   mbti: string;
   size?: 'small' | 'medium';
+  cursor?: boolean;
 }
 
-function MbtiTag({ mbti, size }: Props) {
-  return <CustomChip label={mbti} size={size} mbti={mbti} />;
+function MbtiTag({ mbti, size, cursor }: Props) {
+  return <CustomChip label={mbti} size={size} mbti={mbti} cursor={cursor} />;
 }
 
 export default MbtiTag;
