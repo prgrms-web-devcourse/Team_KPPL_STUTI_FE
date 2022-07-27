b5783712286ed942cdde45d27aa2f7046822f99d
padd60 <padd60@naver.com>
2022-07-28 04:11:45 +0900

    feat : 이미지 타입 추가

---
+++ src/interfaces/assets.d.ts
@@ -0,0 +1,16 @@
+declare module '*.gif' {
+  const value: string;
+  export = value;
+}
+declare module '*.png' {
+  const value: string;
+  export = value;
+}
+declare module '*.jpg' {
+  const value: string;
+  export = value;
+}
+declare module '*.jpeg' {
+  const value: string;
+  export = value;
+}
