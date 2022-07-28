04a1f5eaee1abe07085a4c0ab21700e3fcafe64f
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-07-28 20:59:41 +0900

    feat: TypographyButton 구현
    Feed에 들어있는 underline Typography Button

---
+++ src/components/FeedCard/TypographyButton/style.ts
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material';
+import styled from '@emotion/styled';
+
+import { Props } from './TypographyButton';
+
+export const CustomTypography = styled(Typography)<Props>`
+  display: inline-block;
+  cursor: pointer;
+  margin: ${({ margin }) => margin};
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
