7d9e2afcf370c3d60876e0f57fa7948ee4f017ff
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-07-28 18:17:13 +0900

    feat: 피드 카드 기본 틀 제작(w. Mui Card)

---
+++ src/components/FeedCard/style.ts
@@ -0,0 +1,15 @@
+import Box from '@mui/material/Box';
+import styled from '@emotion/styled';
+
+export const ContentsWrapper = styled.div`               overflow: 'hidden',
+  textOverflow: 'ellipsis';
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  `;
+
+export const FeedContent = styled(Box)`
+  margin: 20px 0 0 16.5;
+  display: flex;
+`;
