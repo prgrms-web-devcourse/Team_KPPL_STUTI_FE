e300b7571b5dbd99ff4fda5f3e4996a66de4646d
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-07-29 01:39:36 +0900

    feat: FeedMenuIconButton 제작
    -MenuList: 수정, 삭제
    - 좌측으로 펼처지게 하기

---
+++ src/components/FeedCard/FeedMenuIconButton/FeedMenuIconButton.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import IconButton from '@mui/material/IconButton';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
+function FeedMenuIconButton() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <div style={{ margin: '100px' }}>
+      <IconButton
+        id='feed-button'
+        aria-controls={open ? 'feed-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup='true'
+        onClick={handleClick}
+      >
+        <MoreHorizIcon></MoreHorizIcon>
+      </IconButton>
+      <Menu
+        id='feed-menu'
+        MenuListProps={{ 'aria-labelledby': 'feed-button' }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem onClick={handleClose}>수정</MenuItem>
+        <MenuItem onClick={handleClose}>삭제</MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+export default FeedMenuIconButton;
