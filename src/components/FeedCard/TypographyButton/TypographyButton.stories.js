983e17f0f042e16c2b9c226958c0ce8a1d75768a
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-07-28 20:59:41 +0900

    feat: TypographyButton 구현
    Feed에 들어있는 underline Typography Button

---
+++ src/components/FeedCard/TypographyButton/TypographyButton.stories.js
@@ -0,0 +1,10 @@
+import TypographyButton from './TypographyButton';
+
+export default {
+  title: 'FeedCard/TypographyButton',
+  component: TypographyButton,
+};
+
+const Default = (args) => <TypographyButton {...args} />;
+
+export const TB = Default.bind({});
