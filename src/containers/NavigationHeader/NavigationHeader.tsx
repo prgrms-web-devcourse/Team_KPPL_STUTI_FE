3964992baeb7042194458c60621b957696a5db26
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2022-07-28 17:50:26 +0900

    feat: Avatar 클릭 시 메뉴 팝업 기능 구현

--- src/containers/NavigationHeader/NavigationHeader.tsx
+++ src/containers/NavigationHeader/NavigationHeader.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
-import { Avatar } from '@mui/material';
+import { useState } from 'react';
+import { Avatar, ClickAwayListener, Menu, MenuItem } from '@mui/material';
 
 import {
   HeaderContainer,
@@ -11,7 +12,19 @@ import {
 } from './style';
 
 function NavigationHeader() {
-  const user = {};
+  const user = { id: 1 };
+  const [el, setEl] = useState<HTMLElement | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setEl(event.currentTarget);
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setEl(null);
+    setIsOpen(false);
+  };
 
   return (
     <header>
@@ -29,8 +42,23 @@ function NavigationHeader() {
             </NavWrapper>
           </NavContainer>
           <LoginWrapper>
-            {!user ? (
-              <Avatar />
+            {user ? (
+              <>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <Avatar alt='User Profile' src='' onClick={handleClick} />
+                </ClickAwayListener>
+                <Menu open={isOpen} anchorEl={el}>
+                  <MenuItem>
+                    <Link to={'#'}>마이페이지</Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link to={'#'}>프로필 수정</Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link to={'#'}>로그아웃</Link>
+                  </MenuItem>
+                </Menu>
+              </>
             ) : (
               <LoginButton>
                 <Link
