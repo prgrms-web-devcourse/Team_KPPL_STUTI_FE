f1d3df8588fe04556c932121fb3dcb18d0034e14
mieumje <66072832+mieumje@users.noreply.github.com>
2022-07-29 02:08:22 +0900

    feat: 스터디 생성 페이지 MbtiSelect 컴포넌트

---
+++ src/components/StudyCreate/StudyCreateMbtiSelect/StudyCreateMbtiSelect.stories.js
@@ -0,0 +1,10 @@
+import StudyCreateMbtiSelect from './StudyCreateMbtiSelect';
+
+export default {
+  title: 'Components/StudyCreate',
+  component: StudyCreateMbtiSelect,
+};
+
+const Template = (args) => <StudyCreateMbtiSelect {...args} />;
+
+export const MbtiSelect = (args) => <StudyCreateMbtiSelect {...args} />;
