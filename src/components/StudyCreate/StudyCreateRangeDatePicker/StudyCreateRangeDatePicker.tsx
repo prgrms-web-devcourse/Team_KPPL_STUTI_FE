c48cb059d4c8cc25843f3e11e0cec91dc90bddc5
mieumje <66072832+mieumje@users.noreply.github.com>
2022-07-30 00:36:08 +0900

    refacotr: 스터디 생성 페이지 RangeDatePicker prop 추가
    시작일, 종료일 value를 가져올 수 있는 함수 prop 추가

--- src/components/StudyCreate/StudyCreateRangeDatePicker/StudyCreateRangeDatePicker.tsx
+++ src/components/StudyCreate/StudyCreateRangeDatePicker/StudyCreateRangeDatePicker.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import moment from 'moment';
 import { DatePicker } from '@mui/x-date-pickers';
 import TextField from '@mui/material/TextField';
@@ -8,12 +8,26 @@ import { RangeDatePickerWRapper } from './style';
 interface Props {
   startLabel: string;
   endLabel: string;
+  getStartValue: (value: string) => void;
+  getEndValue: (value: string) => void;
 }
 
-function StudyCreateRangeDatePicker({ startLabel, endLabel }: Props) {
+function StudyCreateRangeDatePicker({
+  startLabel,
+  endLabel,
+  getStartValue,
+  getEndValue,
+}: Props) {
   const [startDate, setStartDate] = useState(moment());
   const [endDate, setEndDate] = useState(moment().add(7, 'days'));
   const diffDays = moment.duration(startDate.diff(moment())).asDays() + 1;
+
+  useEffect(() => {
+    getStartValue(startDate.format('YYYY-MM-DD hh:mm:ss'));
+  }, [startDate]);
+  useEffect(() => {
+    getEndValue(endDate.format('YYYY-MM-DD hh:mm:ss'));
+  }, [endDate]);
 
   return (
     <RangeDatePickerWRapper>
