df1dbfd203b61f54dd3d631d7ac3ecb9376b7f90
mieumje <66072832+mieumje@users.noreply.github.com>
2022-07-30 01:19:48 +0900

    feat: 스터디 생성 페이지 컴포넌트 export 관리

---
+++ src/components/StudyCreate/index.ts
@@ -0,0 +1,9 @@
+export { default as MbtiRecommend } from './StudyCreateText/StudyCreateMbtiRecommend';
+export { default as Select } from './StudyCreateSelect/StudyCreateSelect';
+export { default as RangeDatePicker } from './StudyCreateRangeDatePicker/StudyCreateRangeDatePicker';
+export { default as RadioGroup } from './StudyCreateRadioGroup/StudyCreateRadioGroup';
+export { default as MbtiSelect } from './StudyCreateMbtiSelect/StudyCreateMbtiSelect';
+export { default as MultiLineInput } from './StudyCreateInput/StudyCreateMultiLineInput';
+export { default as LabelInput } from './StudyCreateInput/StudyCreateLabelInput';
+export { default as Heading } from './StudyCreateHeading/StudyCreateHeading';
+export { default as FileInput } from './StudyCreateFileInput/StudyCreateFileInput';
