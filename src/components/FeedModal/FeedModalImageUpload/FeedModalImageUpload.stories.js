66f3c0e5718b1039e77e707614855a86a1b59567
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-07-29 17:19:17 +0900

    feat: FeedModalImageUpload 기능 구현
    Icon Button을 통해 Image upload해서 Formik으로 확인 가능

---
+++ src/components/FeedModal/FeedModalImageUpload/FeedModalImageUpload.stories.js
@@ -0,0 +1,8 @@
+import FeedModalImageUpload from './FeedModalImageUpload';
+
+export default {
+  title: 'FeedModal/FeedModalImageUpload',
+  component: FeedModalImageUpload,
+};
+
+export const Default = (args) => <FeedModalImageUpload {...args} />;
