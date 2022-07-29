7d479605f30cb7778401e96c6ccc6109797ae6ff
mieumje <66072832+mieumje@users.noreply.github.com>
2022-07-30 00:36:56 +0900

    refactor: 스터디 생성 페이지 LabelInput 컴포넌트 type 변경
    onChange type 변경

--- src/components/StudyCreate/StudyCreateInput/StudyCreateLabelInput.tsx
+++ src/components/StudyCreate/StudyCreateInput/StudyCreateLabelInput.tsx
@@ -4,10 +4,24 @@ interface Props {
   id: string;
   label: string;
   fullWidth?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function StudyCreateLabelInput({ id, label, fullWidth = true }: Props) {
-  return <TextField id={id} label={label} fullWidth={fullWidth} />;
+function StudyCreateLabelInput({
+  id,
+  label,
+  fullWidth = true,
+  onChange,
+}: Props) {
+  return (
+    <TextField
+      id={id}
+      label={label}
+      fullWidth={fullWidth}
+      onChange={onChange}
+      inputProps={{ maxLength: 12, minLength: 5 }}
+    />
+  );
 }
 
 export default StudyCreateLabelInput;
