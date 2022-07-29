9aca5a931b1514f7ff1ee090fae1f77f88d2b4fa
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-07-30 00:34:34 +0900

    feat: Ellipsis ReadMore 기능 & Liked button 활성화
    - Ellipsis 된 Contents 늘리는 Button
    - Liked Button 눌렀을 경우 형태 변화

--- src/components/FeedCard/FeedCard.tsx
+++ src/components/FeedCard/FeedCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   ContentsWrapper,
   FeedBoxWrapper,
@@ -15,6 +15,7 @@ import {
 } from '@mui/material';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 import FeedTypographyButton from './FeedTypographyButton/FeedTypographyButton';
 import FeedMenuIconButton from './FeedMenuIconButton/FeedMenuIconButton';
@@ -24,9 +25,27 @@ export interface BoxProps {
 }
 
 function FeedCard() {
+  const [contentsOpen, setContentsOpen] = useState(false);
+  const [liked, setLiked] = useState(false);
+
   const handlehi = (e: React.MouseEvent<HTMLElement>) => {
     console.log(e.currentTarget);
   };
+
+  const handleReadMore = (e: React.MouseEvent<HTMLElement>) => {
+    setContentsOpen(true);
+  };
+
+  const handleLiked = (e: React.MouseEvent<HTMLElement>) => {
+    //좋아요 부분
+    setLiked(!liked);
+    // if (liked) {
+    //   //좋아요 개수 +=1;
+    // } else {
+    //   //좋아요 개수 -=1;
+    // }
+  };
+
   return (
     <Card sx={{ width: '608px', borderRadius: '8px' }}>
       <CardHeader
@@ -43,7 +62,7 @@ function FeedCard() {
         sx={{ margin: '0.5rem 0 -0.5rem 0' }}
       />
       <CardContent>
-        <ContentsWrapper>
+        {contentsOpen ? (
           <Typography>
             Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
             over medium-high heat. Add chicken, shrimp and chorizo, and cook,
@@ -51,17 +70,26 @@ function FeedCard() {
             Transfer shrimp to a large plate and set aside, leaving chicken and
             chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
             onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.dd rice and stir very gently to
-            distribute. Top with artichokes and peppers, and cook without
-            stirring, until most of the liquid is absorbed, 15 to 18 minutes.
-            Reduce heat to medium-low, add reserved shrimp and mussels, tucking
-            them down into the rice, and cook again without stirring, until
-            mussels have opened and rice is just tender, 5 to 7 minutes more.
-            (Discard any mussels that don&apos;t open.)
+            fragrant, about 10 minutes. Add
           </Typography>
-        </ContentsWrapper>
-        <FeedTypographyButton>더보기</FeedTypographyButton>
+        ) : (
+          <>
+            <ContentsWrapper>
+              <Typography>
+                Heat oil in a (14- to 16-inch) paella pan or a large, deep
+                skillet over medium-high heat. Add chicken, shrimp and chorizo,
+                and cook, stirring occasionally until lightly browned, 6 to 8
+                minutes. Transfer shrimp to a large plate and set aside, leaving
+                chicken and chorizo in the pan. Add pimentón, bay leaves,
+                garlic, tomatoes, onion, salt and pepper, and cook, stirring
+                often until thickened and fragrant, about 10 minutes. Add
+              </Typography>
+            </ContentsWrapper>
+            <FeedTypographyButton onClick={handleReadMore}>
+              더보기
+            </FeedTypographyButton>
+          </>
+        )}
       </CardContent>
       <FeedBoxWrapper margin='1rem 1rem'>
         <CardMedia
@@ -74,8 +102,8 @@ function FeedCard() {
         />
       </FeedBoxWrapper>
       <CardActions disableSpacing>
-        <IconButton aria-label='settings'>
-          <FavoriteBorderIcon />
+        <IconButton aria-label='settings' onClick={handleLiked}>
+          {liked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
         </IconButton>
         <FeedTypographyButton>99(좋아요수)</FeedTypographyButton>
         <FeedTypographyButton margin='0 1rem 0 auto'>
