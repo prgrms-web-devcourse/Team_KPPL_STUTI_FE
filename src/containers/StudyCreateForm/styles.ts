aa72f0be4fa4e43ded2a7fba30c296a1a5d90702
mieumje <66072832+mieumje@users.noreply.github.com>
2022-07-30 01:24:51 +0900

    feat: 스터디 생성 페이지 Form 컨테이너 Wrapper 생성

---
+++ src/containers/StudyCreateForm/styles.ts
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+
+export const StudyCreateWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 3rem 1rem;
+  gap: 2rem;
+`;
+
+export const LocationWrapper = styled.div`
+  display: flex;
+`;
+
+export const MbtiWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MbtiHeadingWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+`;
+
+export const FileUploadWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ImageWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin: 1rem 0;
+`;
