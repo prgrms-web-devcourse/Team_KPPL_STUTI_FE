e12ee9b2f91bcce870ba71cb84ad559e098b8f2f
mieumje <66072832+mieumje@users.noreply.github.com>
2022-07-31 01:31:27 +0900

    feat: 스터디 생성 페이지 Select 컴포넌트 생성

---
+++ src/components/StudyCreate/StudyCreateSelect/StudyCreateSelect.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+
+interface optionsType {
+  value: string | number;
+  label: string;
+}
+
+interface Props {
+  id: string;
+  label: string;
+  fullWidth?: boolean;
+  options: optionsType[];
+  getValue: (value: string | number) => void;
+  disabled?: boolean;
+}
+
+function StudyCreateSelect({
+  id,
+  label,
+  fullWidth = true,
+  options,
+  getValue,
+  disabled,
+}: Props) {
+  const [categoryValue, setCategoryValue] = useState(options[0].value);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setCategoryValue(e.target.value);
+    getValue(e.target.value);
+  };
+
+  return (
+    <TextField
+      select
+      id={id}
+      label={label}
+      value={categoryValue}
+      fullWidth={fullWidth}
+      onChange={handleChange}
+      disabled={disabled}
+    >
+      {options.map((option) => (
+        <MenuItem key={option.value} value={option.value}>
+          {option.label}
+        </MenuItem>
+      ))}
+    </TextField>
+  );
+}
+
+export default StudyCreateSelect;
