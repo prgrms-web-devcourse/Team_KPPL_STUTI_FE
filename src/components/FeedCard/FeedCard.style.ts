1b9a230dc04f11d5dd082703dc1397a7c14a6524
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-07-30 20:41:59 +0900

    refactor: 사용하지 않는 함수 제거

--- src/components/FeedCard/FeedCard.style.ts
+++ src/components/FeedCard/FeedCard.style.ts
@@ -1,7 +1,4 @@
-import Box from '@mui/material/Box';
 import styled from '@emotion/styled';
-
-import { BoxProps } from './FeedCard';
 
 export const ContentsWrapper = styled.div`
   overflow: 'hidden',
@@ -11,7 +8,3 @@ export const ContentsWrapper = styled.div`
   -webkit-line-clamp: 4;
   -webkit-box-orient: vertical;
   `;
-
-export const FeedBoxWrapper = styled(Box)<BoxProps>`
-  margin: ${({ margin }) => margin};
-`;
