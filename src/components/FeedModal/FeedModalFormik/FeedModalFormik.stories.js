48210a3b433f7aec5e473182f516802c543d0c35
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-07-30 17:58:21 +0900

    chore: 안쓰는 파일 삭제

--- src/components/FeedModal/FeedModalFormik/FeedModalFormik.stories.js
+++
@@ -1,8 +0,0 @@
-import FeedModalFormik from './FeedModalFormik';
-
-export default {
-  title: 'FeedModal/FeedModalFormik',
-  component: FeedModalFormik,
-};
-
-export const Default = (args) => <FeedModalFormik {...args} />;
