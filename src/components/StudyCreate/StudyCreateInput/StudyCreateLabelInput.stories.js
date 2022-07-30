a475b8f477b8a2056b3f8295fd887026bb9abf14
mieumje <66072832+mieumje@users.noreply.github.com>
2022-07-31 01:26:23 +0900

    feat: 스터디 생성 페이지 Input 컴포넌트 생성
    일반 Input 컴포넌트, Multi Line Input 컴포넌트

---
+++ src/components/StudyCreate/StudyCreateInput/StudyCreateLabelInput.stories.js
@@ -0,0 +1,15 @@
+import StudyCreateLabelInput from './StudyCreateLabelInput';
+
+export default {
+  title: 'Components/StudyCreate',
+  component: StudyCreateLabelInput,
+};
+
+const Template = (args) => <StudyCreateLabelInput {...args} />;
+
+export const LabelInput = Template.bind({});
+LabelInput.args = {
+  id: 'study-title',
+  label: '스터디 제목',
+  fullWidth: true,
+};
