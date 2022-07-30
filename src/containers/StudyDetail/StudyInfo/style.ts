15e337ffb40c0f4dfced0d82a8a1e8f64ee03db8
padd60 <padd60@naver.com>
2022-07-30 22:21:15 +0900

    feat : 스터디 상세페이지 스터디 정보 컨테이너 추가

---
+++ src/containers/StudyDetail/StudyInfo/style.ts
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+
+export const StudyInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  background-color: #f9fafb;
+  padding: 20px;
+`;
+
+export const StudyInfoWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+export const StudyInfoIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  color: #6b7280;
+`;
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,3 +1,4 @@
 export { default as SampleCounter } from './Counter/Counter';
 export { default as StudyCreateForm } from './StudyCreateForm/StudyCreateForm';
 export { default as StudyDetailMbtiRecommand } from './StudyDetail/MbtiRecommand/MbtiRecommend';
+export { default as StudyDetailStudyInfo } from './StudyDetail/StudyInfo/StudyInfo';
