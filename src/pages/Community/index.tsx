f8159e704553769ca605855e39b2c1ef038450c8
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-08-01 23:26:47 +0900

    feat: Community Router 연결

---
+++ src/pages/Community/index.tsx
@@ -0,0 +1,8 @@
+import FeedModal from '@src/containers/FeedModal/FeedModal';
+import FeedCard from '@src/components/FeedCard/FeedCard';
+
+function Community() {
+  return <FeedModal></FeedModal>;
+}
+
+export default Community;
