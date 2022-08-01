17a35d9623750462cfd26f1790e6e59cebd0739b
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2022-08-02 03:51:32 +0900

    feat: 회원가입 폼 기본 기능 구현

--- src/containers/SignUpForm/SignUpForm.tsx
+++ src/containers/SignUpForm/SignUpForm.tsx
@@ -1,4 +1,5 @@
-import { Button, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+import { Button, MenuItem, TextField, Typography } from '@mui/material';
 
 import {
   FormContainer,
@@ -7,21 +8,83 @@ import {
   MbtiWrapper,
   StyledForm,
 } from './style';
+import { careers, jobs, mbtis } from './options';
 
 function SignUpForm() {
+  const [nickname, setNickname] = useState('');
+  const [job, setJob] = useState('');
+  const [career, setCareer] = useState('');
+  const [mbti, setMbti] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`nickname: ${nickname}`);
+    console.log(`job: ${job}`);
+    console.log(`career: ${career}`);
+    console.log(`mbti: ${mbti}`);
+  };
+
   return (
     <FormContainer>
-      <StyledForm>
+      <StyledForm onSubmit={handleSubmit}>
         <Typography variant='body2' align='center'>
           계정 프로필을 설정해 주세요
         </Typography>
         <FormInputContainer>
-          <TextField label='닉네임' required />
-          <TextField label='직무' required select />
-          <TextField label='경력' required select />
+          <TextField
+            label='닉네임'
+            required
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+          />
+          <TextField
+            label='직무'
+            required
+            select
+            onChange={(e) => {
+              setJob(e.target.value);
+            }}
+            value={job}
+          >
+            {jobs.map((job) => (
+              <MenuItem key={job.value} value={job.value}>
+                {job.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            label='경력'
+            required
+            select
+            onChange={(e) => {
+              setCareer(e.target.value);
+            }}
+            value={career}
+          >
+            {careers.map((career) => (
+              <MenuItem key={career.value} value={career.value}>
+                {career.label}
+              </MenuItem>
+            ))}
+          </TextField>
           <MbtiContainer>
             <MbtiWrapper>
-              <TextField label='MBTI' required select fullWidth />
+              <TextField
+                label='MBTI'
+                required
+                select
+                onChange={(e) => {
+                  setMbti(e.target.value);
+                }}
+                value={mbti}
+                fullWidth
+              >
+                {mbtis.map((mbti) => (
+                  <MenuItem key={mbti} value={mbti}>
+                    {mbti}
+                  </MenuItem>
+                ))}
+              </TextField>
             </MbtiWrapper>
             <MbtiWrapper>
               <Button size='large' fullWidth>
@@ -29,7 +92,7 @@ function SignUpForm() {
               </Button>
             </MbtiWrapper>
           </MbtiContainer>
-          <Button size='large' fullWidth>
+          <Button size='large' fullWidth type='submit'>
             가입
           </Button>
         </FormInputContainer>
---
+++ src/containers/SignUpForm/options.ts
@@ -0,0 +1,68 @@
+export const jobs = [
+  {
+    value: 'FRONTEND',
+    label: '프론트엔드',
+  },
+  {
+    value: 'BACKEND',
+    label: '백엔드',
+  },
+  {
+    value: 'INFRA',
+    label: '인프라',
+  },
+  {
+    value: 'IOS',
+    label: 'IOS',
+  },
+  {
+    value: 'ANDROID',
+    label: '안드로이드',
+  },
+  {
+    value: 'DATA_ANALYST',
+    label: '데이터 분석가',
+  },
+  {
+    value: 'DEVELOPER',
+    label: '개발자',
+  },
+];
+
+export const careers = [
+  {
+    value: 'JUNIOR',
+    label: '0년차 ~ 3년차',
+  },
+  {
+    value: 'INTERMEDIATE',
+    label: '3년차 ~ 5년차',
+  },
+  {
+    value: 'SENIOR',
+    label: '5년차 ~ 10년차',
+  },
+  {
+    value: 'MASTER',
+    label: '10년차 ~',
+  },
+];
+
+export const mbtis = [
+  'ENTJ',
+  'ENFJ',
+  'ENFP',
+  'ENTP',
+  'ESFP',
+  'ESFJ',
+  'ESTP',
+  'ESTJ',
+  'INFP',
+  'INFJ',
+  'INTP',
+  'ISTP',
+  'ISFP',
+  'ISFJ',
+  'ISTJ',
+  'INTJ',
+];
