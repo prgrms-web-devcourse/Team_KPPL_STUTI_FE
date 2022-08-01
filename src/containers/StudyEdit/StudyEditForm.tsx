29835e654eb63e83c476333e47218783fdfa0854
mieumje <66072832+mieumje@users.noreply.github.com>
2022-08-01 17:39:25 +0900

    feat: 스터디 수정 페이지 Form 컨테이너

---
+++ src/containers/StudyEdit/StudyEditForm.tsx
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react';
+import {
+  MultiLineInput,
+  LabelInput,
+  FileInput,
+} from '@src/components/StudyCreate&Edit';
+import { fetchStudyDetails } from '@src/api/fetchStudyDetails';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+import {
+  StudyEditHeading,
+  StudyEditWrapper,
+  StudyEditImageWrapper,
+  ImageWrapper,
+  ButtonWrapper,
+  StudyDescriptionWrapper,
+} from './style';
+
+function StudyEditForm() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imageSrc, setImageSrc] = useState<File | null>(null);
+  const [thumbnailImage, setThumbnailImage] = useState<string>('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchStudyDetails();
+      const { title, description, imageUrl } = data;
+
+      setTitle(title);
+      setDescription(description);
+      setThumbnailImage(imageUrl);
+    };
+
+    fetchData();
+  }, []);
+
+  const encodeFile = (fileBlob: File) => {
+    const reader = new FileReader();
+    if (!fileBlob) return;
+    reader.readAsDataURL(fileBlob);
+
+    return new Promise<void>((resolve) => {
+      reader.onload = () => {
+        const result = reader.result as string;
+        setThumbnailImage(result);
+
+        resolve();
+      };
+    });
+  };
+
+  const onImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const maxSize = 1 * 1024 * 1024;
+      const fileSize = e.target.files[0].size;
+
+      if (fileSize > maxSize) {
+        alert('첨부 파일은 최대 1MB 입니다.');
+        return;
+      }
+      setImageSrc(e.target.files[0]);
+      encodeFile(e.target.files[0]);
+    }
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('제목 ' + title);
+    console.log('이미지 ' + imageSrc);
+    console.log('내용' + description);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <StudyEditWrapper>
+        <StudyEditHeading>
+          <Typography variant='h4'>스터디 수정</Typography>
+          <LabelInput
+            id='study-title'
+            label='스터디명'
+            value={title}
+            fullWidth={true}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+        </StudyEditHeading>
+        <StudyEditImageWrapper>
+          <Typography variant='h5'>대표 이미지</Typography>
+          <ImageWrapper>
+            {thumbnailImage && (
+              <img
+                src={thumbnailImage}
+                alt='study-image'
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  borderRadius: '0.5rem',
+                  objectFit: 'cover',
+                }}
+              />
+            )}
+          </ImageWrapper>
+          <ButtonWrapper>
+            <FileInput message='이미지 업로드' onChange={onImageChange} />
+          </ButtonWrapper>
+        </StudyEditImageWrapper>
+        <StudyDescriptionWrapper>
+          <Typography variant='h5'>상세 설명</Typography>
+          <MultiLineInput
+            id='study-description'
+            placeholder='스터디 내용을 기재해주세요.'
+            value={description}
+            height='600px'
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+        </StudyDescriptionWrapper>
+        <Button type='submit'>제출</Button>
+      </StudyEditWrapper>
+    </form>
+  );
+}
+
+export default StudyEditForm;
---
+++ src/containers/StudyEdit/style.ts
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+
+export const StudyEditWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 3rem 1rem;
+  gap: 2rem;
+`;
+
+export const StudyEditHeading = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const StudyEditImageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const ImageWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const StudyDescriptionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
