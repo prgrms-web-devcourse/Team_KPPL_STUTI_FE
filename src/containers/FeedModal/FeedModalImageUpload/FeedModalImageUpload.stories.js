446ccde1e039c950dab8be53c90cb762d052d53a
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-08-02 01:09:21 +0900

    chore: 파일 제거됨(FeedModal)

--- src/containers/FeedModal/FeedModalImageUpload/FeedModalImageUpload.stories.js
+++
@@ -1,8 +0,0 @@
-import FeedModalImageUpload from './FeedModalImageUpload';
-
-export default {
-  title: 'FeedModal/FeedModalImageUpload',
-  component: FeedModalImageUpload,
-};
-
-export const Default = (args) => <FeedModalImageUpload {...args} />;
