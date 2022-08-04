a2478243723389833a20c498703f7b7c6f52b631
mieumje <66072832+mieumje@users.noreply.github.com>
2022-08-04 18:23:46 +0900

    feat, refactor: form validation 추가, state, 함수 정리
    title, description validation 추가
    그 외 필드(topic, 지역, location, image)는 커스텀
    
    title, body local state 삭제
    errorMessage local state 추가
    
    onImageChange 함수 수정
    errorMessage handle useEffect 추가
    topic, region, recruitsNumber state handle 함수 추가 및 적용
    FormData 생성 함수 추가

--- src/containers/StudyCreateForm/StudyCreateForm.tsx
+++ src/containers/StudyCreateForm/StudyCreateForm.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import * as Yup from 'yup';
+import React, { useRef, useState, useEffect } from 'react';
+import { Formik, Field } from 'formik';
 import {
   MbtiRecommend,
   RangeDatePicker,
@@ -15,13 +17,21 @@ import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
 
 import {
+  ButtonWrapper,
+  CameraIcon,
+  ErrorMessage,
   FileUploadWrapper,
+  ImageContainer,
   ImageWrapper,
+  Image,
+  InputWrapper,
   LocationWrapper,
   MbtiHeadingWrapper,
   MbtiWrapper,
+  PeopleWrapper,
   StudyCreateWrapper,
   StudyDescriptionWrapper,
+  TopicWrapper,
 } from './style';
 
 const options = [
@@ -142,19 +152,53 @@ const people = [
   },
 ];
 
+const FILE_SIZE = 1 * 1024 * 1024;
+const SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg', 'image/gif', 'image/png'];
+
+const CreateSchema = Yup.object({
+  title: Yup.string()
+    .max(50, '50자를 넘을 수 없습니다.')
+    .min(5, '최소 5자 이상입니다.')
+    .required('제목을 입력해주세요.'),
+  image: Yup.mixed()
+    .test('fileSize', '파일 크기는 최대 1MB 입니다.', (value) => {
+      if (value) {
+        return value.size <= FILE_SIZE;
+      } else {
+        return true;
+      }
+    })
+    .test('fileFormat', '파일 형식이 올바르지 않습니다.', (value) => {
+      if (value) {
+        return SUPPORTED_FORMATS.includes(value.type);
+      } else {
+        return true;
+      }
+    }),
+  description: Yup.string()
+    .max(1000, '1,000자를 넘을 수 없습니다.')
+    .min(10, '최소 10자 이상입니다.')
+    .required('스터디 내용을 입력해주세요.'),
+});
+
 function StudyCreateFormContainer() {
-  const [title, setTitle] = useState('');
   const [topic, setTopic] = useState('');
   const [recruitsNumber, setRecruitsNumber] = useState('');
-  const [regions, setRegions] = useState('');
+  const [region, setRegion] = useState('');
   const [isOnline, setIsOnline] = useState(true);
-  const [startDate, setStartDate] = useState<string | number>('');
-  const [endDate, setEndDate] = useState<string | number>('');
+  const [startDate, setStartDate] = useState<string>('');
+  const [endDate, setEndDate] = useState<string>('');
   const [mbtiPreference, setMbtiPreference] = useState(true);
   const [mbtiCheckedList, setMbtiCheckedList] = useState<string[]>([]);
   const [imageSrc, setImageSrc] = useState<File | null>(null);
   const [thumbnailImage, setThumbnailImage] = useState<string>('');
-  const [body, setBody] = useState('');
+  const [fileErrorMessage, setFileErrorMessage] = useState<string>('');
+  const [topicErrorMessage, setTopicErrorMessage] = useState<string>('');
+  const [peopleErrorMessage, setPeopleErrorMessage] = useState<string>('');
+  const titleRef = useRef<null | HTMLDivElement>(null);
+  const descriptionRef = useRef<null | HTMLDivElement>(null);
+  const topicRef = useRef<null | HTMLDivElement>(null);
+  const peopleRef = useRef<null | HTMLDivElement>(null);
 
   const onMbtiSelectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const checkedMbti = e.target.value;
@@ -183,17 +227,21 @@ function StudyCreateFormContainer() {
   };
 
   const onImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      const maxSize = 1 * 1024 * 1024;
-      const fileSize = e.target.files[0].size;
+    const { files } = e.target;
 
-      if (fileSize > maxSize) {
-        alert('첨부 파일은 최대 1MB 입니다.');
-        return;
-      }
-      setImageSrc(e.target.files[0]);
-      encodeFile(e.target.files[0]);
+    if (!files || !files[0]) return;
+
+    const maxSize = 1 * 1024 * 1024;
+    const fileSize = files[0].size;
+
+    if (fileSize > maxSize) {
+      setFileErrorMessage('파일 크기는 최대 1MB 입니다.');
+      return;
     }
+
+    setImageSrc(files[0]);
+    encodeFile(files[0]);
+    setFileErrorMessage('');
   };
 
   const isOnlineChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -204,132 +252,261 @@ function StudyCreateFormContainer() {
     }
   };
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(topic, regions, recruitsNumber);
+  useEffect(() => {
+    if (topic) setTopicErrorMessage('');
+  }, [topic]);
+
+  useEffect(() => {
+    if (recruitsNumber) setPeopleErrorMessage('');
+  }, [recruitsNumber]);
+
+  const checkSelectOptions = () => {
+    if (!topic) {
+      setTopicErrorMessage('분야를 선택해주세요');
+    }
+    if (!recruitsNumber) {
+      setPeopleErrorMessage('인원수를 선택해주세요');
+    }
   };
 
-  const getStartDate = (value: string | number) => {
+  const handleTopic = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setTopic(value);
+  };
+  const handleRegion = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setRegion(value);
+  };
+  const handleRecruitsNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setRecruitsNumber(value);
+  };
+
+  const getStartDate = (value: string) => {
     setStartDate(value);
   };
-  const getEndDate = (value: string | number) => {
+  const getEndDate = (value: string) => {
     setEndDate(value);
   };
 
+  const createFormData = (title: string, description: string) => {
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('topic', topic);
+    formData.append('isOnline', isOnline.toString());
+    formData.append('region', isOnline ? '' : region);
+    formData.append('numberOfRecruits', recruitsNumber);
+    formData.append('startDateTime', startDate.toString());
+    formData.append('endDateTime', endDate.toString());
+    formData.append('preferredMBTIs', JSON.stringify(mbtiCheckedList));
+    formData.append('imageFile', imageSrc ? imageSrc : '');
+    formData.append('description', description);
+    return formData;
+  };
+
   return (
-    <form onSubmit={onSubmit}>
-      <StudyCreateWrapper>
-        <Typography variant='h4'>스터디 생성</Typography>
-        <LabelInput
-          id='study-title'
-          label='제목'
-          fullWidth={true}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-        />
-        <Select
-          id='study-category'
-          label='분야'
-          options={options}
-          value={topic}
-          fullWidth={true}
-          handleChange={(e) => {
-            setTopic(e.target.value);
-          }}
-        />
-        <LocationWrapper>
-          <RadioGroup
-            defaultValue={radioValues[0].value}
-            labels={radioValues}
-            name='study-location'
-            row={true}
-            onChange={isOnlineChange}
-          />
-          <Select
-            id='study-location'
-            label='지역'
-            options={locations}
-            value={regions}
-            fullWidth={true}
-            handleChange={(e) => {
-              setRegions(e.target.value);
-            }}
-            disabled={isOnline ? true : false}
-          />
-        </LocationWrapper>
-        <Select
-          id='study-recruits-number'
-          label='인원'
-          value={recruitsNumber}
-          options={people}
-          fullWidth={true}
-          handleChange={(e) => {
-            setRecruitsNumber(e.target.value);
-          }}
-        />
-        <RangeDatePicker
-          startLabel='시작일'
-          endLabel='종료일'
-          getStartValue={getStartDate}
-          getEndValue={getEndDate}
-        />
-        <MbtiWrapper>
-          <MbtiHeadingWrapper>
-            <Typography variant='h5'>선호 MBTI</Typography>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  value='non-preference'
-                  onChange={() => {
-                    setMbtiPreference(!mbtiPreference);
-                  }}
-                  disabled={mbtiCheckedList.length > 0 ? true : false}
+    <Formik
+      initialValues={{
+        title: '',
+        description: '',
+      }}
+      validationSchema={CreateSchema}
+      onSubmit={(values, actions) => {
+        actions.setSubmitting(true);
+        const formData = createFormData(values.title, values.description);
+
+        setTimeout(() => {
+          for (const key of formData.keys()) {
+            console.log(key, ':', formData.get(key));
+          }
+          actions.setSubmitting(false);
+        }, 3000);
+      }}
+    >
+      {(formik) => {
+        return (
+          <form onSubmit={formik.handleSubmit}>
+            <StudyCreateWrapper>
+              <Typography variant='h4'>스터디 생성</Typography>
+              <InputWrapper>
+                {formik.touched.title && formik.errors.title && (
+                  <ErrorMessage ref={titleRef}>
+                    {(() => {
+                      if (titleRef.current) {
+                        titleRef.current.scrollIntoView({
+                          behavior: 'smooth',
+                          block: 'center',
+                        });
+                      }
+                      return formik.errors.title;
+                    })()}
+                  </ErrorMessage>
+                )}
+                <Field
+                  as={LabelInput}
+                  label='스터디명'
+                  id='title'
+                  {...formik.getFieldProps('title')}
                 />
-              }
-              label='선택 안함'
-            />
-          </MbtiHeadingWrapper>
-          <MbtiRecommend mbtis={['INFJ', 'ISFP', 'ENFP']} />
-          <MbtiSelect
-            onChange={onMbtiSelectChange}
-            disabled={mbtiPreference ? false : true}
-            limit={mbtiCheckedList.length}
-            checkedList={mbtiCheckedList}
-          />
-        </MbtiWrapper>
-        <Typography variant='h5'>대표 이미지</Typography>
-        <FileUploadWrapper>
-          <ImageWrapper>
-            {imageSrc && (
-              <img
-                src={thumbnailImage}
-                alt='study-image'
-                style={{
-                  width: '100%',
-                  height: '100%',
-                  borderRadius: '0.5rem',
-                  objectFit: 'cover',
-                }}
+              </InputWrapper>
+              <TopicWrapper>
+                {topicErrorMessage && (
+                  <ErrorMessage ref={topicRef}>
+                    {(() => {
+                      if (!titleRef.current && topicRef.current) {
+                        topicRef.current.scrollIntoView({
+                          behavior: 'smooth',
+                          block: 'center',
+                        });
+                      }
+                      return topicErrorMessage;
+                    })()}
+                  </ErrorMessage>
+                )}
+                <Select
+                  id='topic'
+                  label='분야'
+                  value={topic}
+                  options={options}
+                  fullWidth={true}
+                  handleChange={handleTopic}
+                />
+              </TopicWrapper>
+              <LocationWrapper>
+                <RadioGroup
+                  defaultValue={radioValues[0].value}
+                  labels={radioValues}
+                  name='isOnline'
+                  row={true}
+                  onChange={isOnlineChange}
+                />
+                <Select
+                  id='region'
+                  label='지역'
+                  options={locations}
+                  value={region}
+                  fullWidth={true}
+                  handleChange={handleRegion}
+                  disabled={isOnline ? true : false}
+                />
+              </LocationWrapper>
+              <PeopleWrapper>
+                {peopleErrorMessage && (
+                  <ErrorMessage ref={peopleRef}>
+                    {(() => {
+                      if (
+                        !titleRef.current &&
+                        !topicRef.current &&
+                        peopleRef.current
+                      ) {
+                        peopleRef.current.scrollIntoView({
+                          behavior: 'smooth',
+                          block: 'center',
+                        });
+                      }
+                      return peopleErrorMessage;
+                    })()}
+                  </ErrorMessage>
+                )}
+                <Select
+                  id='study-number-of-people'
+                  label='인원'
+                  options={people}
+                  value={recruitsNumber}
+                  fullWidth={true}
+                  handleChange={handleRecruitsNumber}
+                />
+              </PeopleWrapper>
+              <RangeDatePicker
+                startLabel='시작일'
+                endLabel='종료일'
+                getStartValue={getStartDate}
+                getEndValue={getEndDate}
               />
-            )}
-          </ImageWrapper>
-          <FileInput message='이미지 업로드' onChange={onImageChange} />
-        </FileUploadWrapper>
-        <StudyDescriptionWrapper>
-          <Typography variant='h5'>상세 설명</Typography>
-          <MultiLineInput
-            id='study-body'
-            placeholder='스터디 내용을 기재해주세요.'
-            height='600px'
-            onChange={(e) => {
-              setBody(e.target.value);
-            }}
-          />
-        </StudyDescriptionWrapper>
-        <Button type='submit'>제출</Button>
-      </StudyCreateWrapper>
-    </form>
+              <MbtiWrapper>
+                <MbtiHeadingWrapper>
+                  <Typography variant='h5'>선호 MBTI</Typography>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        value='non-preference'
+                        onChange={() => {
+                          setMbtiPreference(!mbtiPreference);
+                        }}
+                        disabled={mbtiCheckedList.length > 0 ? true : false}
+                      />
+                    }
+                    label='선택 안함'
+                  />
+                </MbtiHeadingWrapper>
+                {/*TODO : 해당 User의 추천 MBTI 로직 */}
+                <MbtiRecommend mbtis={['INFJ', 'ISFP', 'ENFP']} />
+                <MbtiSelect
+                  onChange={onMbtiSelectChange}
+                  disabled={mbtiPreference ? false : true}
+                  limit={mbtiCheckedList.length}
+                  checkedList={mbtiCheckedList}
+                />
+              </MbtiWrapper>
+              <FileUploadWrapper>
+                <Typography variant='h5'>대표 이미지</Typography>
+                <ImageContainer>
+                  {fileErrorMessage && (
+                    <ErrorMessage>{fileErrorMessage}</ErrorMessage>
+                  )}
+                  <ImageWrapper>
+                    {imageSrc ? (
+                      <Image src={thumbnailImage} alt='study-image' />
+                    ) : (
+                      <CameraIcon color='secondary' />
+                    )}
+                  </ImageWrapper>
+                </ImageContainer>
+                <ButtonWrapper>
+                  <FileInput message='이미지 업로드' onChange={onImageChange} />
+                </ButtonWrapper>
+              </FileUploadWrapper>
+              <StudyDescriptionWrapper>
+                <Typography variant='h5'>상세 설명</Typography>
+                {formik.touched.description && formik.errors.description && (
+                  <ErrorMessage ref={descriptionRef}>
+                    {(() => {
+                      if (
+                        !titleRef.current &&
+                        !topicRef.current &&
+                        !peopleRef.current &&
+                        descriptionRef.current
+                      ) {
+                        descriptionRef.current.scrollIntoView({
+                          behavior: 'smooth',
+                          block: 'start',
+                        });
+                      }
+                      return formik.errors.description;
+                    })()}
+                  </ErrorMessage>
+                )}
+                <Field
+                  as={MultiLineInput}
+                  label='상세 설명'
+                  id='description'
+                  placeholder='스터디 내용을 기재해주세요.'
+                  height='600px'
+                  {...formik.getFieldProps('description')}
+                />
+              </StudyDescriptionWrapper>
+              <Button
+                type='submit'
+                onClick={checkSelectOptions}
+                disabled={formik.isSubmitting}
+              >
+                제출
+              </Button>
+            </StudyCreateWrapper>
+          </form>
+        );
+      }}
+    </Formik>
   );
 }
 
