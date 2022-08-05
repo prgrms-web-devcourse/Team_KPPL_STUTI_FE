144250b13421b9effd3980e3bfb7e40cd339b059
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2022-08-05 22:14:35 +0900

    style: 기존 Layout 스타일 제거 후 LayoutContainer 스타일 지정

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router';
 import NavigationHeader from '@src/containers/NavigationHeader/NavigationHeader';
 
+import { LayoutContainer } from './style';
+
 function Layout() {
   return (
-    <>
+    <LayoutContainer>
       <NavigationHeader />
       <main>
         <Outlet />
       </main>
-    </>
+    </LayoutContainer>
   );
 }
 
---
+++ src/layout/style.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const LayoutContainer = styled.div`
+  max-width: 640px;
+  margin: 0 auto;
+`;
