0fa5220c2c333a6e061d74f8480edc2b66c941bf
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-08-08 03:55:21 +0900

    feat: Comment 부분 추가(StudyDetailStudyQuestion)

--- src/containers/CommunityPostListSection/CommunityPost/CommunityPost.tsx
+++ src/containers/CommunityPostListSection/CommunityPost/CommunityPost.tsx
@@ -1,5 +1,15 @@
 import { Link } from 'react-router-dom';
-import React, { useState, useRef, useLayoutEffect, forwardRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import React, {
+  useState,
+  useRef,
+  useLayoutEffect,
+  forwardRef,
+  useEffect,
+} from 'react';
+import { AxiosError, AxiosResponse } from 'axios';
+import { selectQuestion, setQuestions } from '@store/slices/question';
+import { errorType } from '@src/interfaces/error';
 import CommunityPostTypographyButton from '@src/containers/CommunityPostListSection/CommunityPostTypographyButton/CommunityPostTypographyButton';
 import CommunityPostMenuIconButton from '@src/containers/CommunityPostListSection/CommunityPost/CommunityPostMenuIconButton';
 import {
@@ -18,11 +28,15 @@ import {
 } from '@mui/material';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import { studyDetailQuestionType } from '@interfaces/studyDetailQuestion';
 import { CommunityPostType } from '@interfaces/community';
+import { StudyDetailStudyQuestion } from '@containers';
+import { getStudyQuestionInformation } from '@apis/studyDetail';
 import {
   postCommunityPostLikeApi,
   deleteCommunityPostLikeApi,
 } from '@apis/community';
+
 const CommunityPost = forwardRef<any, CommunityPostType>(function CommunityPost(
   {
     postId,
@@ -40,7 +54,12 @@ const CommunityPost = forwardRef<any, CommunityPostType>(function CommunityPost(
 ) {
   const [liked, setLiked] = useState({ check: false, count: 0 });
   const [isExpand, setIsExpand] = useState<string | number>('none');
+  const [isComment, setIsComment] = useState(false);
+  const [commentsError, setCommentsError] = useState(false);
+
   const contentsRef = useRef<HTMLInputElement>(null);
+  const postComments = useSelector(selectQuestion);
+  const dispatch = useDispatch();
 
   useLayoutEffect(() => {
     setLiked({ check: isliked, count: totalLikes });
@@ -48,11 +67,39 @@ const CommunityPost = forwardRef<any, CommunityPostType>(function CommunityPost(
       const contentsHeight = contentsRef.current.getBoundingClientRect().height;
       setIsExpand(contentsHeight > 96 ? 4 : 'none');
     }
+
+    const fetchComments = async () => {
+      setCommentsError(false);
+      try {
+        const res: studyDetailQuestionType = await getStudyQuestionInformation(
+          postId,
+          5,
+        );
+        dispatch(setQuestions(res));
+      } catch (error) {
+        setCommentsError(true);
+        console.error(error);
+        const { response } = error as AxiosError;
+        const { data }: { data: errorType } = response as AxiosResponse;
+        const { errorCode } = data;
+      }
+    };
+    (async () => {
+      await Promise.all([fetchComments()]);
+    })();
   }, []);
 
-  const handleReadMore = () => {
-    setIsExpand('none');
-  };
+  useEffect(() => {
+    if (!commentsError) return;
+
+    const timeOutId = setTimeout(() => {
+      setCommentsError(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeOutId);
+    };
+  }, [commentsError]);
 
   const handleLiked = async (e: React.MouseEvent<HTMLElement>) => {
     if (liked.check) {
@@ -63,6 +110,7 @@ const CommunityPost = forwardRef<any, CommunityPostType>(function CommunityPost(
       await postCommunityPostLikeApi(`posts/${postId}/like`);
     }
   };
+
   return (
     <Card ref={ref}>
       <CardHeader
@@ -102,7 +150,7 @@ const CommunityPost = forwardRef<any, CommunityPostType>(function CommunityPost(
           <Typography ref={contentsRef}>{contents}</Typography>
         </ContentsWrapper>
         {isExpand !== 'none' && (
-          <CommunityPostTypographyButton onClick={handleReadMore}>
+          <CommunityPostTypographyButton onClick={() => setIsExpand('none')}>
             더보기
           </CommunityPostTypographyButton>
         )}
@@ -124,10 +172,23 @@ const CommunityPost = forwardRef<any, CommunityPostType>(function CommunityPost(
         <CommunityPostTypographyButton>
           {liked.count}
         </CommunityPostTypographyButton>
-        <CommunityPostTypographyButton name='댓글' margin='0 1rem 0 auto'>
+        <CommunityPostTypographyButton
+          onClick={() => setIsComment(!isComment)}
+          name='댓글'
+          margin='0 1rem 0 auto'
+        >
           {totalComments}
         </CommunityPostTypographyButton>
       </CardActions>
+      {isComment && (
+        <Box sx={{ margin: '-1.5rem 1rem 1rem' }}>
+          <StudyDetailStudyQuestion
+            {...postComments}
+            size={5}
+            study_id={postId}
+          />
+        </Box>
+      )}
     </Card>
   );
 });
