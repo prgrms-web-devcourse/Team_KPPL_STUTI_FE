408a2503858ce8db4765e316659af16a54e94312
DESKTOP-H8C5V9L\ekzm5 <jaeung5169@naver.com>
2022-08-10 22:49:51 +0900

    feat: 회원가입 페이지 Formik 적용

--- src/containers/SignUp/Form/Form.tsx
+++ src/containers/SignUp/Form/Form.tsx
@@ -1,6 +1,8 @@
+import * as yup from 'yup';
 import { useNavigate } from 'react-router';
 import { useDispatch } from 'react-redux';
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { useFormik } from 'formik';
 import { setStorageItem } from '@src/utils/storage';
 import { getQueryString } from '@src/utils/queryString';
 import { loginUser } from '@src/store/slices/user';
@@ -22,33 +24,46 @@ function Form() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const email = getQueryString('email', 'string') + '';
-  const [nickname, setNickname] = useState(
-    getQueryString('name', 'string') + '',
-  );
-  const [field, setField] = useState('');
-  const [career, setCareer] = useState('');
-  const [mbti, setMbti] = useState('');
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = {
-      email,
-      nickname,
-      field,
-      career,
-      MBTI: mbti,
-    };
-
-    console.log(formData);
-    // const data = await signUp(formData);
-    // dispatch(loginUser(data.member));
-    // setStorageItem('token', data.accesstoken);
-    // navigate('/', { replace: true });
-  };
+  const {
+    values,
+    touched,
+    errors,
+    isSubmitting,
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    setFieldValue,
+  } = useFormik({
+    initialValues: {
+      nickname: '',
+      field: '',
+      career: '',
+      MBTI: '',
+    },
+    validationSchema: yup.object({
+      nickname: yup
+        .string()
+        .trim('앞, 뒤 공백을 제거해주세요')
+        .strict()
+        .max(16, '닉네임은 최대 16글자 까지 가능합니다.')
+        .required('닉네임을 입력해 주세요.'),
+      field: yup.string().required('직무를 입력해주세요'),
+      career: yup.string().required('경력을 입력해주세요'),
+      MBTI: yup.string().required('MBTI를 입력해주세요.'),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
 
   const handleClick = () => {
     window.open(MBTI_TEST_URL);
   };
+
+  useEffect(() => {
+    setFieldValue('nickname', getQueryString('name', 'string'));
+  }, []);
 
   return (
     <FormContainer>
@@ -58,33 +73,46 @@ function Form() {
         </Typography>
         <FormInputContainer>
           <TextField
-            label='닉네임'
-            required
-            value={nickname}
-            onChange={(e) => setNickname(e.target.value)}
+            id='nickname'
+            name='nickname'
+            label='닉네임*'
+            value={values.nickname}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.nickname && Boolean(errors.nickname)}
+            helperText={touched.nickname && errors.nickname}
           />
           <Select
-            label='직무'
-            onChange={(e) => setField(e.target.value)}
-            required
+            id='field'
+            name='field'
+            label='직무*'
+            onChange={handleChange}
             options={jobs}
-            value={field}
+            value={values.field}
+            error={touched.field && Boolean(errors.field)}
+            helperText={touched.field && errors.field}
           />
           <Select
-            label='경력'
-            onChange={(e) => setCareer(e.target.value)}
-            required
+            id='career'
+            name='career'
+            label='경력*'
+            onChange={handleChange}
             options={careers}
-            value={career}
+            value={values.career}
+            error={touched.career && Boolean(errors.career)}
+            helperText={touched.career && errors.career}
           />
           <MbtiContainer>
             <MbtiWrapper>
               <Select
+                id='MBTI'
+                name='MBTI'
                 label='MBTI'
-                onChange={(e) => setMbti(e.target.value)}
-                required
+                onChange={handleChange}
                 options={mbtis}
-                value={mbti}
+                value={values.MBTI}
+                error={touched.MBTI && Boolean(errors.MBTI)}
+                helperText={touched.MBTI && errors.MBTI}
                 fullWidth
               />
             </MbtiWrapper>
@@ -94,7 +122,7 @@ function Form() {
               </Button>
             </MbtiWrapper>
           </MbtiContainer>
-          <Button size='large' fullWidth type='submit'>
+          <Button size='large' type='submit' disabled={isSubmitting} fullWidth>
             가입
           </Button>
         </FormInputContainer>
