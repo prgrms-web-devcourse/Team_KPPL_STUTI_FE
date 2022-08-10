184ad5592bab32b0f2bb37f6bdcea3daaeaaadcc
lexie-kaia <kaia.gayeon.kim@gmail.com>
2022-08-11 03:57:25 +0900

    feat: members api 수정

--- src/apis/members.ts
+++ src/apis/members.ts
@@ -1,9 +1,42 @@
 import axiosInstance from '@apis/axiosInstance';
 
-export const getUserProfile = async ({ userId }: { userId: number }) => {
+const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
+
+export const getUserProfile = async (userId: number) => {
+  const url = `${API_ENDPOINT}/api/v1/members/${userId}`;
+
   const { data } = await axiosInstance({
-    url: '/mock/userProfile.json',
     method: 'GET',
+    url,
   });
+
   return data;
+};
+
+export const updateUserProfile = async (
+  userId: number,
+  data: {
+    profileImageUrl?: string;
+    nickname?: string;
+    field?: string;
+    career?: string;
+    MBTI?: string;
+    githubUrl?: string;
+    blogUrl?: string;
+  },
+) => {
+  const url = `${API_ENDPOINT}/api/v1/members/${userId}`;
+
+  const res = await axiosInstance({
+    // API 수정 중
+    method: 'PATCH',
+    url,
+    data,
+    headers: {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2Iiwicm9sZXMiOiJST0xFX01FTUJFUiIsImlhdCI6MTY2MDAyMzIyOCwiZXhwIjoxNjYzMDIzMjI4fQ.lequMTJd6iooVkkY7-f7ALWTSg7qabGZLuM-uiRoWZs',
+    },
+  });
+
+  console.log(res);
 };
