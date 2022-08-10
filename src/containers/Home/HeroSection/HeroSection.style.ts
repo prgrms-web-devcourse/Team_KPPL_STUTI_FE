2222de3b2f8f734435585bdd0a2a171a172b8f67
lexie-kaia <kaia.gayeon.kim@gmail.com>
2022-08-10 14:41:44 +0900

    feat: Hero 컨텐츠 추가

--- src/containers/Home/HeroSection/HeroSection.style.ts
+++ src/containers/Home/HeroSection/HeroSection.style.ts
@@ -4,15 +4,23 @@ export const Section = styled.section`
   padding: 1rem 1rem 0 1rem;
 `;
 
-export const Imgcontainer = styled.div`
-  height: 328px;
-  border-radius: 0.5rem;
+export const Banner = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   overflow: hidden;
-  background-color: ${({ theme }) => theme.palette.grey[200]};
+  padding: 1rem;
+
+  @media (max-width: 568px) {
+    flex-direction: column;
+  }
 `;
 
-export const Img = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+export const Title = styled.div`
+  padding-top: 2rem;
+
+  @media (max-width: 568px) {
+    text-align: center;
+  }
 `;
