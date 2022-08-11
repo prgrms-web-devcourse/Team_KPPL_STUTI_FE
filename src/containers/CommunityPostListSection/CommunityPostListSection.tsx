147d0507175b4788d47da0575cef8e6346df9f1d
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-08-11 20:10:40 +0900

    refactor: observer 변경

--- src/containers/CommunityPostListSection/CommunityPostListSection.tsx
+++ src/containers/CommunityPostListSection/CommunityPostListSection.tsx
@@ -2,15 +2,23 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 import { addPost, selectPost, setPost } from '@store/slices/post';
 import CircularProgress from '@mui/material/CircularProgress';
+import { useInterSectionObserver } from '@hooks/useIntersectionObserver';
 import { CommunityPostWrapper } from '@containers/CommunityPostListSection/CommunityPostListSection.style';
 import CommunityPost from '@containers/CommunityPostListSection/CommunityPost/CommunityPost';
 import { ItemCard } from '@components';
 import { getCommunityDataApi } from '@apis/community';
-
 function CommunityPostListSection() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const [postTarget, setPostTarget] = useState();
+
+  const { targetRef } = useInterSectionObserver({
+    onTargetObserve: async () => {
+      if (!post.value.posts) return;
+      const lastPostId = post.value.posts.at(-1)?.postId;
+      const res = await getCommunityDataApi(5, lastPostId);
+      dispatch(addPost(res));
+    },
+  });
 
   const dispatch = useDispatch();
   const post = useSelector(selectPost);
@@ -30,43 +38,24 @@ function CommunityPostListSection() {
     })();
   }, []);
 
-  useEffect(() => {
-    let io: any;
-    if (postTarget) {
-      io = new IntersectionObserver(handleInfiniteScroll);
-      io.observe(postTarget);
-    }
-    return () => io && io.disconnect();
-  }, [postTarget]);
-
-  const handleInfiniteScroll = async (entries: any) => {
-    if (!entries[0].isIntersecting || !post.value.posts || !post.value.hasNext)
-      return;
-    const lastPostId = post.value.posts.at(-1)?.postId;
-    const res = await getCommunityDataApi(5, lastPostId);
-    console.log(res);
-    dispatch(addPost(res));
-  };
-
   return (
     <CommunityPostWrapper>
-      {post.value.posts &&
-        post.value.posts.map((post) => (
-          <CommunityPost
-            key={post.postId}
-            postId={post.postId}
-            memberId={post.memberId}
-            nickname={post.nickname}
-            mbti={post.mbti}
-            updatedAt={post.updatedAt}
-            profileImageUrl={post.profileImageUrl}
-            contents={post.contents}
-            postImageUrl={post.postImageUrl}
-            likedMembers={post.likedMembers}
-            totalPostComments={post.totalPostComments}
-            ref={setPostTarget}
-          />
-        ))}
+      {post.value.posts.map((post) => (
+        <CommunityPost
+          key={post.postId}
+          postId={post.postId}
+          memberId={post.memberId}
+          nickname={post.nickname}
+          mbti={post.mbti}
+          updatedAt={post.updatedAt}
+          profileImageUrl={post.profileImageUrl}
+          contents={post.contents}
+          postImageUrl={post.postImageUrl}
+          likedMembers={post.likedMembers}
+          totalPostComments={post.totalPostComments}
+          ref={targetRef}
+        />
+      ))}
       {!loading && !error && !post.value.posts && (
         <ItemCard>커뮤니티가 없습니다.</ItemCard>
       )}
