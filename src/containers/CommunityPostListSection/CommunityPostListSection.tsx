e7bb2624e4afb4740a301ad8f79534e4e60283a5
GeonWooPaeng <geonwoopaeng@gmail.com>
2022-08-12 06:29:35 +0900

    fix: 중복 무한 스크롤 고치기

--- src/containers/CommunityPostListSection/CommunityPostListSection.tsx
+++ src/containers/CommunityPostListSection/CommunityPostListSection.tsx
@@ -1,27 +1,33 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 import { addPost, selectPost, setPost } from '@store/slices/post';
+import { Box } from '@mui/material';
 import { useInterSectionObserver } from '@hooks/useIntersectionObserver';
 import { CommunityPostWrapper } from '@containers/CommunityPostListSection/CommunityPostListSection.style';
 import CommunityPost from '@containers/CommunityPostListSection/CommunityPost/CommunityPost';
 import { ItemCard, SkeletonPost } from '@components';
 import { getCommunityDataApi } from '@apis/community';
+
 function CommunityPostListSection() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
   const dispatch = useDispatch();
-  const post = useSelector(selectPost);
+  const {
+    value: { posts, hasNext },
+  } = useSelector(selectPost);
 
   const { targetRef } = useInterSectionObserver({
     onTargetObserve: async () => {
-      if (!post.value.posts || !post.value.hasNext) return;
-      const lastPostId = post.value.posts.at(-1)?.postId;
+      setLoading(true);
       try {
-        setLoading(true);
-        const res = await getCommunityDataApi(5, lastPostId);
-        if (!res) return;
-        dispatch(addPost(res));
+        if (!posts || !hasNext) return;
+        const lastPostId = posts[posts.length - 1].postId;
+
+        if (!loading) {
+          const res = await getCommunityDataApi(5, lastPostId);
+          dispatch(addPost(res));
+        }
       } catch (e) {
         console.error(e);
       } finally {
@@ -47,23 +53,27 @@ function CommunityPostListSection() {
 
   return (
     <CommunityPostWrapper>
-      {post.value.posts.map((post) => (
-        <CommunityPost
+      {posts.map((post, index) => (
+        <Box
+          ref={index === posts.length - 1 ? targetRef : null}
           key={post.postId}
-          postId={post.postId}
-          memberId={post.memberId}
-          nickname={post.nickname}
-          mbti={post.mbti}
-          updatedAt={post.updatedAt}
-          profileImageUrl={post.profileImageUrl}
-          contents={post.contents}
-          postImageUrl={post.postImageUrl}
-          likedMembers={post.likedMembers}
-          totalPostComments={post.totalPostComments}
-          ref={targetRef}
-        />
+        >
+          <CommunityPost
+            key={post.postId}
+            postId={post.postId}
+            memberId={post.memberId}
+            nickname={post.nickname}
+            mbti={post.mbti}
+            updatedAt={post.updatedAt}
+            profileImageUrl={post.profileImageUrl}
+            contents={post.contents}
+            postImageUrl={post.postImageUrl}
+            likedMembers={post.likedMembers}
+            totalPostComments={post.totalPostComments}
+          />
+        </Box>
       ))}
-      {!loading && !error && !post.value.posts && (
+      {!loading && !error && !posts && (
         <ItemCard>커뮤니티가 없습니다.</ItemCard>
       )}
       {error && (
